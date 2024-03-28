6f5d688d8a4fc960968c507225923aa4058f7ecb
codingcreedd <moubayedmarwancoding@gmail.com>
2024-03-28 08:47:11 +0200

    Config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'hover-nav': 'blue-500',
+      },
+    },
+  },
+  variants: {
+    textColor: ['hover'],
+  },
+  plugins: [],
+}
